9a7743eb033728c0bd8fe664135004f4477d6d1e
Srinu Reddy <srinureddynani18580.s@gmail.com>
2020-05-06 15:12:48 +0530

    Initial Commit

---
+++ testprojectforGIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testprojectforGIT
+//
+//  Created by Dharmani Apps on 06/05/20.
+//  Copyright © 2020 Dharmani Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
